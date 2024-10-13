ab011af531821dcc3b994d43191154c397d127b9
Chetan Sharma <chetansharma911912@gmail.com>
2024-10-13 15:47:32 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { checkPrime } from 'crypto';
+
 
 @Component({
   selector: 'app-reactive-forms',
@@ -9,8 +12,9 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './reactive-forms.component.css'
 })
 export class ReactiveFormsComponent {
+
+  // nx=["1","2"];
   countries = [
-    '--- Select ---',
     'India',
     'USA',
     'UK',
@@ -24,29 +28,85 @@ export class ReactiveFormsComponent {
     'Dancing'
   ];
 
-  reactiveForm: FormGroup = new FormGroup({
-    firstname : new FormControl("", Validators.required),
-    lastname : new FormControl("", Validators.required),
-    password : new FormControl("", Validators.required),
-    email: new FormControl("", Validators.required),
-    country: new FormControl("", Validators.required),
-    document: new FormControl("", Validators.required),
-    mobile: new FormControl("", Validators.required),
-    address: new FormControl("", Validators.required),
-    age: new FormControl("", Validators.required),
-    language: new FormControl("", Validators.required),
-    hobbies :new FormControl("", Validators.required),
-    gender :new FormControl("Male", Validators.required),
-    human:new FormControl("", Validators.required),
-    numbers:new FormControl("1", Validators.required),
+  numbers =  [
+    { name: '1',  checked: false },
+    { name: '2',  checked: false },
+  ]
 
-  });
+  reactiveForm: FormGroup =new FormGroup({
+    firstname : new FormControl("", [
+      Validators.required, 
+      Validators.pattern(/^[a-zA-Z ]+$/gm),
+      Validators.minLength(3), 
+      
+    ]),
+    lastname : new FormControl("", [
+      Validators.required, 
+      Validators.pattern(/^[a-zA-Z ]+$/gm),
+      Validators.minLength(3), 
+      
+    ]),
+    password : new FormControl("",
+      [
+        Validators.required, 
+        Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/),
+        Validators.minLength(8),
+        
+      ]
+    ),
+    email: new FormControl("", [
+        Validators.required, 
+        Validators.email
+      ]
+    ),
+    country: new FormControl(
+      "",
+      Validators.required
+    ),
+    document: new FormControl("", Validators.required),
+    mobile: new FormControl("", [
+        Validators.required, 
+        Validators.pattern(/^[0-9]+$/gm)
+      ]
+    ),
+    address: new FormControl("", [
+      Validators.required
+    ]),
+    age: new FormControl("", Validators.required),
+    language: new FormControl(["Tamil"], Validators.required),
+    hobbies :new FormControl("", Validators.required),
+    gender : new FormControl("", Validators.required),
+    human: new FormControl("You are Human"),
+    choice :new FormControl("Enjoyment", Validators.required),
+    numbers: new FormArray([
+    ],Validators.required),
+
+  });;
+
+  
 
   ngOnInit(){
-    // console.log("Chetan");
+  }
+
+  addnumbers(event:any){
+    let checked = event.target.checked;
+    let value = event.target.value;
+    const x = this.reactiveForm.get('numbers') as FormArray;
+    if(checked){
+      x.push(new FormControl(value));
+    }
+    if(!checked){
+      // x.remove(new FormControl(value));
+    }
+  }
+
+  showErrorMessage(field:string){
+    return (this.reactiveForm.get(field)?.invalid && this.reactiveForm.get(field)?.touched)
   }
 
   formSubmit(){
-
+    // if(this.reactiveForm.valid){
+      console.log(this.reactiveForm);
+    // }
   }
 }
